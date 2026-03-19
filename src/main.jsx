4607d5fa101b917d630340f2e06df6aaa5479363
Alex Curran <alexcurran.va@gmail.com>
2026-03-19 15:07:18 -0500

    Adjust import and swap `element` for `Component` browswer router object property

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
+import { createBrowserRouter, RouterProvider } from "react-router";
+// import { RouterProvider } from "react-router/dom";
 import "./index.css";
 import App from "./App.jsx";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <App />,
+        Component: App,
     },
 ]);
 const root = document.getElementById("root");
