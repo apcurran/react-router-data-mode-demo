4a3d255aec1df73d630d63bcef62e6b2963a0528
Alex Curran <alexcurran.va@gmail.com>
2026-03-24 16:56:07 -0500

    Add Link router component to enable client-side routing to each unique post based on post id

--- src/components/ui/PostItem.jsx
+++ src/components/ui/PostItem.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router";
+
 function PostItem({ post }) {
     return (
         <li className="post-item">
             <article>
-                <h2>{post.title}</h2>
-                <p>{post.body}</p>
+                <Link to={`/posts/${post.id}`}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                </Link>
             </article>
         </li>
     );
