08510773a608f0490ec97317cc473ef6b29c41ef
Alex Curran <alexcurran.va@gmail.com>
2026-04-09 13:44:23 -0500

    Layout and styling updates for form popover on adding a post

--- src/components/ui/AddPostPopover.jsx
+++ src/components/ui/AddPostPopover.jsx
@@ -50,25 +50,31 @@ function AddPostPopover() {
             <div id="post-form-popover" popover="auto" ref={popoverRef} className="form-popover">
                 <form action={formAction}>
                     <h3>New Post</h3>
-                    <input
-                        name="title"
-                        placeholder="Title"
-                        type="text"
-                        required
-                        disabled={isPending}
-                    />
-                    <textarea
-                        name="body"
-                        placeholder="Your content here..."
-                        required
-                        disabled={isPending}
-                    ></textarea>
-                    <button type="submit" disabled={isPending}>
-                        {isPending ? "Saving Post..." : "Create Post"}
-                    </button>
-                    <button type="button" onClick={() => popoverRef.current.hidePopover()}>
-                        Cancel
-                    </button>
+                    <div className="form-group">
+                        <input
+                            name="title"
+                            placeholder="Title"
+                            type="text"
+                            required
+                            disabled={isPending}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <textarea
+                            name="body"
+                            placeholder="Your content here..."
+                            required
+                            disabled={isPending}
+                        ></textarea>
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" disabled={isPending}>
+                            {isPending ? "Saving Post..." : "Create Post"}
+                        </button>
+                        <button type="button" onClick={() => popoverRef.current.hidePopover()}>
+                            Cancel
+                        </button>
+                    </div>
                     <div className="status-feeback">
                         {state.success ? (
                             <p className="success-message">Success! ID: {state.data.id}</p>
